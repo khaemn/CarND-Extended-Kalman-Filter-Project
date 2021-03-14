84335308a6f9d12363ffbe1a9feb3e1e980f0ddf
Volodymyr Havrylov <khaemn@gmail.com>
2021-03-14 18:33:00 +0200

    Calculate Jacobian added

--- src/tools.cpp
+++ src/tools.cpp
@@ -39,9 +39,44 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   return rmse;
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state)
+{
+  MatrixXd Hj(3, 4);
+
+  // recover state parameters
+  const double px = x_state(0);
+  const double py = x_state(1);
+  const double vx = x_state(2);
+  const double vy = x_state(3);
+
+  // check division by zero
+  if ((px == 0.) && (py == 0.))
+  {
+    cout << "Error! Both Px and Py are 0" << endl;
+    return Hj;
+  }
+
+  const double squared_sum   = px * px + py * py;
+  const double kappa         = 1. / sqrt(squared_sum);
+  const double recipr_sq_sum = 1. / squared_sum;
+  const double beta          = pow(squared_sum, 3. / 2.);
+  const double kappa_px      = kappa * px;
+  const double kappa_py      = kappa * py;
+
+  Hj(0, 0) = kappa_px;
+  Hj(0, 1) = kappa_py;
+  Hj(0, 2) = 0.;
+  Hj(0, 3) = 0.;
+
+  Hj(1, 0) = -1. * py * recipr_sq_sum;
+  Hj(1, 1) = px * recipr_sq_sum;
+  Hj(1, 2) = 0.;
+  Hj(1, 3) = 0.;
+
+  Hj(2, 0) = (vx * py - vy * px) * py / beta;
+  Hj(2, 1) = (vy * px - vx * py) * px / beta;
+  Hj(2, 2) = kappa_px;
+  Hj(2, 3) = kappa_py;
+
+  return Hj;
 }
